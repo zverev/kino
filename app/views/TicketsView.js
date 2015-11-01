963d6fd7bbdfd952d12d4d27876bfada709ad276
zverev <alexander.zverev@gmail.com>
2015-11-01 15:25:40 +0300

    TicketsView: idle

--- app/views/TicketsView.js
+++ app/views/TicketsView.js
@@ -1,6 +1,12 @@
-define(['marionette', 'text!views/ticketsView.html'], function(Marionette, tpl) {
+define(['marionette', 'text!views/ticketsView.html', 'text!views/ticketsIdleView.html'], function(Marionette, tpl, tplIdle) {
     return Marionette.ItemView.extend({
         className: 'ticketsView',
-        template: _.template(tpl)
+        render: function() {
+            this.$el.html(_.template(tplIdle)(this.model.attributes));
+            setTimeout(function() {
+                this.$el.html(_.template(tpl)(this.model.attributes))
+            }.bind(this), 3000);
+            return this;
+        }
     })
 });
